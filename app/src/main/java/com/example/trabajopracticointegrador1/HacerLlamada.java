bd6df3a8c99b269775aacc07481c6220a47686b2
AballayCristianAlejandro <64992973+AballayCristianAlejandro@users.noreply.github.com>
2021-03-17 18:41:14 -0300

    agregue  un if en el BrodcastReceiver para que no llama constantemente

--- app/src/main/java/com/example/trabajopracticointegrador1/HacerLlamada.java
+++ app/src/main/java/com/example/trabajopracticointegrador1/HacerLlamada.java
@@ -4,21 +4,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.view.View;
 
-import static android.content.Intent.ACTION_CALL;
+
 
 public class HacerLlamada extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-
-            Intent i = new Intent(Intent.ACTION_CALL);
-            i.setData(Uri.parse("tel: 911"));
-
+        boolean estado= intent.getAction().equals(Intent.ACTION_POWER_CONNECTED);
+        if (estado) {
+            Intent i = new Intent(Intent.ACTION_CALL,Uri.parse("tel: 911"));
             context.startActivity(i);
-
+        }
     }
 }
 
